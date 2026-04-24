888d12bfc9e4e3f4c3b1ffb2bb3d37f9e02b4614
Maxim <teakeyx@gmail.com>
2026-04-24 20:00:06 +0300

    replace vite api url to .env

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     strictPort: true,
     proxy: {
       '/api': {
-        target: 'http://expire-share:8080',
+        target: import.meta.env.VITE_API_URL,
         changeOrigin: true,
         rewrite: path => path.replace(/^\/api/, ''),
       },
